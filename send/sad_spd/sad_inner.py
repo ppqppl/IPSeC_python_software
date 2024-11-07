6762dde9b851d5df1ef17be8895c2c374abc73f4
ppqppl <1683127550@qq.com>
2024-11-07 18:38:14 +0800

    m300_python

--- send/sad_spd/sad_inner.py
+++ send/sad_spd/sad_inner.py
@@ -1,6 +1,8 @@
 from scapy.all import *
 import sys
 
+from PowerOnScripts.sad_spd_python.ubuntu.sad_mgnt_service import whole_pkt
+
 sys.path.append('../')
 sys.path.append('../../')
 sys.path.append('../../utils')
@@ -11,10 +13,15 @@ from send_pkt import *
 cur_seq = 0
 
 total_pkt_cnt = 0
+sad_index_num = 1
+
+sad_pkt_list = []
 
 class sad_pkt:
+    cue_seq = 0
     op_code = "ADD"
     dst_mac = "08:11:22:33:44:ab"
+    src_mac = "08:11:22:33:44:ab"
     dst_chip_id = "06"
     dst_mode_id = "02"
     src_chip_id = "03"
@@ -27,7 +34,7 @@ class sad_pkt:
     sa_status = 0 # 0-Unnegotiate; 1-Negotiating; 2-In Service;
     sa_anti_replay = 0 # 0-Disable anti-replay; 1-Enable anti-replay;
     sa_type = 0 # 0-Unicast; 1-Multicast; 2-Management; 3-Neotiation;
-    sa_op_code = 0 # 1-Direct; 2-Drop; 3-Encryption
+    sa_op_code = 3 # 1-Direct; 2-Drop; 3-Encryption
     sa_packet_mtu = 1536
     channel_id = 0 # 0-Normal; 1-CPU; 2-Loop;
     transmit_mode = 1 # 1-Tunnel; 2-Transmission;
@@ -41,9 +48,67 @@ class sad_pkt:
     firewall_smask = "255.255.255.255"
     firewall_dport = 456
     firewall_sport = 123
-    firewall_protocol = "UDP"
-    def __init__(self,dst_mode,opcode,tunnel_dip,tunnel_sip):
+    protocol = "UDP"
+    message_head = b''
+    mgnt_dma_route_header = b''
+    messgae_load = b''
+    whole_pkt = b''
+    # def __init__(self,op_code,sa_index,key_index,src_mode,dst_mode,tunnel_dip,tunnel_sip,protocol):
+    #     self.sa_index = sa_index
+    #     self.key_index = key_index
+    #     self.dst_chip_id = dst_mode[0:2:1]
+    #     self.dst_mode_id = dst_mode[2:4:1]
+    #     self.src_chip_id = src_mode[0:2:1]
+    #     self.src_mode_id = src_mode[2:4:1]
+    #     self.op_code = op_code
+    #     self.tunnel_dip = tunnel_dip
+    #     self.tunnel_sip = tunnel_sip
+    #     self.protocol = protocol
+    def __init__(self,tunnel_dip, tunnel_sip, sa_index, key_index, protocol):
+        global sad_index_num
 
+
+
+    def set_sa_type(self,sa_type):
+        self.sa_type = sa_type
+    def set_sa_op_code(self,sa_op_code):
+        self.sa_op_code = sa_op_code
+    def config_message_head(self):
+        self.message_head += (str_2_hexbytes(macstr_2_str(self.dst_mac)) + get_mac_address() + str_2_hexbytes("1000") + self.cue_seq.to_bytes(2,byteorder='big'))
+    def config_mgnt_dma_route_header(self):
+        blank = 0
+        self.mgnt_dma_route_header += (str_2_hexbytes(self.dst_chip_id) + str_2_hexbytes(self.dst_mode_id) + str_2_hexbytes(self.src_chip_id) +
+                                       str_2_hexbytes(self.src_mode_id) + blank.to_bytes(12,byteorder='big'))
+    def config_messgae_load(self):
+        blank = 0
+        self.messgae_load += (str_2_hexbytes("0100") + str_2_hexbytes(op_code_to_str(op_code)) + str_2_hexbytes(str(sa_valid)) + get_sa_index(self.sa_index) +
+                              str_2_hexbytes(ipstr_2_hexstr(self.tunnel_dip)) + str_2_hexbytes(ipstr_2_hexstr(self.tunnel_sip)) + str_2_hexbytes(ipstr_2_hexstr(self.firewall_dip)) +
+                              str_2_hexbytes(ipstr_2_hexstr(self.firewall_sip)) + str_2_hexbytes(ipstr_2_hexstr(self.firewall_dmask)) + str_2_hexbytes(ipstr_2_hexstr(self.firewall_smask)) +
+                              get_port(self.firewall_dport) + get_port(self.firewall_sport) + str_2_hexbytes(protocol_to_str(self.protocol)) + blank.to_bytes(11,byteorder='big'))
+    def config_whole_pkt(self):
+        self.whole_pkt = self.message_head + self.mgnt_dma_route_header + self.messgae_load
+
+def get_mac_address():
+    mac = uuid.UUID(int = uuid.getnode())
+    mac_str = str(mac).split('-')[-1]
+    res = bytes()
+    for i in range(0,6):
+        res += (int(mac_str[i*2:i*2+2],16)).to_bytes(1,byteorder='big')
+    return res
+
+def get_sa_index(data):
+    data_hex = hex(data)
+    data_str = str(data_hex)[2:]
+    data_str_reserve = data_str[::-1]
+    data_out_reserve = data_str_reserve.ljust(8,'0')
+    return str_2_hexbytes(data_out_reserve[::-1])
+
+def get_port(data):
+    data_hex = hex(data)
+    data_str = str(data_hex)[2:]
+    data_str_reserve = data_str[::-1]
+    data_out_reserve = data_str_reserve.ljust(4,'0')
+    return str_2_hexbytes(data_out_reserve[::-1])
 
 def op_code_to_str(op_code):
     if op_code == 'ADD':
@@ -67,6 +132,22 @@ def protocol_to_str(protocol):
     else:
         return "00"
 
-def send_sad(whole_pkt):
+def set_sad_pkt(tunnel_dip,tunnel_sip,sa_index,key_index,protocol):
+    length = len(protocol)
+
+
+def send_sad_sa_0(sad_pkt_obj):
+    sad_pkt_obj.set_sa_type(0)
+    sad_pkt_obj.set_sa_op_code(2)
+    whole_pkt = sad_pkt_obj.whole_pkt
     hex_dump(whole_pkt)
     sendp(whole_pkt,iface='以太网')
+
+
+def send_sad(sad_pkt_obj):
+    whole_pkt = sad_pkt_obj.whole_pkt
+    hex_dump(whole_pkt)
+    sendp(whole_pkt,iface='以太网')
+
+
+
