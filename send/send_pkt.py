25ba353ee25c929873cfc4b8c45a01c072c3ab08
ppqppl <1683127550@qq.com>
2024-11-12 14:16:15 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	send/sad_spd/sad_inner.py

--- send/send_pkt.py
+++ send/send_pkt.py
@@ -13,20 +13,20 @@ import negotiation
 
 cur_seq = 0
 
-def send_negotiation_phase_1(pkt_obj):
-    print("Send negotiation phase 1")
-    dst_mac = str_2_hexbytes(pkt_obj.dst_mac)
-    whole_pkt = dst_mac
-    pkt_obj.set_whole_pkt(whole_pkt)
-    send_pkt(pkt_obj.whole_pkt)
-
-def send_negotiation_phase_2(pkt_obj):
-    print("Send negotiation phase 2")
-    send_pkt(pkt_obj.whole_pkt)
-
-def send_negotiation_phase_3(pkt_obj):
-    print("Send negotiation phase 3")
-    send_pkt(pkt_obj.whole_pkt)
+# def send_negotiation_phase_1(pkt_obj):
+#     print("Send negotiation phase 1")
+#     dst_mac = str_2_hexbytes(pkt_obj.dst_mac)
+#     whole_pkt = dst_mac
+#     pkt_obj.set_whole_pkt(whole_pkt)
+#     send_pkt(pkt_obj.whole_pkt)
+#
+# def send_negotiation_phase_2(pkt_obj):
+#     print("Send negotiation phase 2")
+#     send_pkt(pkt_obj.whole_pkt)
+#
+# def send_negotiation_phase_3(pkt_obj):
+#     print("Send negotiation phase 3")
+#     send_pkt(pkt_obj.whole_pkt)
 
 def send_sad_pkt(tunnel_dip,tunnel_sip,sa_index,key_index,protocol,dst_chip):
     sad.set_sad_pkt(tunnel_dip,tunnel_sip,sa_index,key_index,protocol,dst_chip)
