fb1f3dd3272e6dd88c21410758bb15db641a0bee
ppqppl <1683127550@qq.com>
2024-11-13 15:55:06 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	send/sad_spd/sad_inner.py

--- rev/rev_pkt.py
+++ rev/rev_pkt.py
@@ -6,7 +6,7 @@ from scapy.layers.l2 import Ether
 
 sys.path.append("../")
 sys.path.append("../sql_console")
-# import utils
+import utils
 from sqlite3_utils import *
 
 def callback_sniff_1000(pkt_data):
@@ -18,13 +18,14 @@ def callback_sniff_1000(pkt_data):
     dst_mode_id = data[3]
     src_chip_id = data[4]
     src_mode_id = data[5]
+    op_mode = data[6]
     seq = data[0] + data[1]
-    pkt_obj = pkt_class(dst_mac,src_mac,pkt_data)
-    pkt_obj.set_dst_chip_id(dst_chip_id)
-    pkt_obj.set_dst_mode_id(dst_mode_id)
-    pkt_obj.set_src_chip_id(src_chip_id)
-    pkt_obj.set_src_mode_id(src_mode_id)
-    pkt_obj.set_seq(seq)
+    pkt_data = data[18:]
+    hex_dump(pkt_data)
+    whole_pkt = ""
+    whole_pkt[20] = '01' if op_mode == "01" else 0
+
+    send_pkt(whole_pkt)
 
 def rev_pkt_1000(threadname):
     print("Thread " + threadname + " start!!")
@@ -34,7 +35,7 @@ def rev_pkt_1000(threadname):
     # packet = sniff(filter = "arp")
     # print(packet)
 
-def rev_pkt_arp(threadname):
+# def rev_pkt_arp(threadname):
 
 
 def pkt_judge(pkt_data):
@@ -59,5 +60,8 @@ def pkt_judge(pkt_data):
     elif dst_chip_id == b'\x03':
         print("MMU")
 
-rev_pkt_1000('a')
-# print_str("hello")
+def send_pkt(whole_pkt):
+    hex_dump(whole_pkt)
+    send(whole_pkt,iface='WLAN')
+
+# rev_pkt_1000('a')
