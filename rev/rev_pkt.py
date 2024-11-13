f1a9b061591407850055b9ccc96148c872fa5fbf
ppqppl <1683127550@qq.com>
2024-11-13 11:45:56 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	send/sad_spd/sad_inner.py

--- rev/rev_pkt.py
+++ rev/rev_pkt.py
@@ -9,38 +9,33 @@ sys.path.append("../sql_console")
 # import utils
 from sqlite3_utils import *
 
-
-
 def callback_sniff_1000(pkt_data):
     # pkt_data.show()
-    if pkt_data.haslayer(IP):
-        pkt_data_ip = pkt_data[IP]
-        if hasattr(pkt_data_ip,'proto'):
-            print(pkt_data[IP].proto)
-        else:
-            print("no proto")
-    # dst_mac = pkt_data['Ethernet'].dst
-    # src_mac = pkt_data['Ethernet'].src
-    # data = pkt_data['Raw'].load
-    # dst_chip_id = data[2]
-    # dst_mode_id = data[3]
-    # src_chip_id = data[4]
-    # src_mode_id = data[5]
-    # seq = data[0] + data[1]
-    # pkt_obj = pkt_class(dst_mac,src_mac,pkt_data)
-    # pkt_obj.set_dst_chip_id(dst_chip_id)
-    # pkt_obj.set_dst_mode_id(dst_mode_id)
-    # pkt_obj.set_src_chip_id(src_chip_id)
-    # pkt_obj.set_src_mode_id(src_mode_id)
-    # pkt_obj.set_seq(seq)
+    dst_mac = pkt_data['Ethernet'].dst
+    src_mac = pkt_data['Ethernet'].src
+    data = pkt_data['Raw'].load
+    dst_chip_id = data[2]
+    dst_mode_id = data[3]
+    src_chip_id = data[4]
+    src_mode_id = data[5]
+    seq = data[0] + data[1]
+    pkt_obj = pkt_class(dst_mac,src_mac,pkt_data)
+    pkt_obj.set_dst_chip_id(dst_chip_id)
+    pkt_obj.set_dst_mode_id(dst_mode_id)
+    pkt_obj.set_src_chip_id(src_chip_id)
+    pkt_obj.set_src_mode_id(src_mode_id)
+    pkt_obj.set_seq(seq)
 
-def rev_socket_1000(threadname):
+def rev_pkt_1000(threadname):
     print("Thread " + threadname + " start!!")
     print(11)
-    # packet = sniff(iface='WLAN',filter="ether proto 0x1000",count=1,prn=callback_sniff_1000)
+    packet = sniff(iface='WLAN',filter="ether proto 0x1000",prn=callback_sniff_1000)
     # packet = sniff(iface='WLAN',prn = callback_sniff_1000)
-    packet = sniff(filter = "ARP")
+    # packet = sniff(filter = "arp")
     # print(packet)
+
+def rev_pkt_arp(threadname):
+
 
 def pkt_judge(pkt_data):
     global dst_mode_id
@@ -64,5 +59,5 @@ def pkt_judge(pkt_data):
     elif dst_chip_id == b'\x03':
         print("MMU")
 
-rev_socket_1000('a')
+rev_pkt_1000('a')
 # print_str("hello")
